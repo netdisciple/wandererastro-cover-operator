6db68a691b05d55c6b7605e218c654347915f6cc
netdisciple <michael.deviveiros@gmail.com>
2025-03-02 04:11:08 +0100

    dynamic ttyusb discovery added

--- menu.sh
+++ menu.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+TTYUSB=$(dmesg | grep ch341 | grep now | awk '{print $NF}')
+if [[ -n "$TTYUSB" ]]; then 
+	TTYUSB="/dev/${TTYUSB}"
+	echo "TTYUSB: ${TTYUSB}"
+else
+	echo "Something is wrong..."
+	exit 1
+fi
+
 printmenu() {
 	echo "__        __              _                     _        _             "
 	echo "\ \      / /_ _ _ __   __| | ___ _ __ ___ _ __ / \   ___| |_ _ __ ___  "
@@ -14,27 +23,34 @@ printmenu() {
 	echo " "
 	echo "WandererAstro Cover Operator"
 	echo " "
+	echo "Looks like Wanderer is on ${TTYUSB}"
+	echo " "
 	echo "Select Action"
 	echo " "
 	echo "1) Open"
 	echo "2) Close"
 	echo "3) Open, Light Off"
 	echo "4) Close, Light On"
+	echo "5) Light Off"
+	echo "6) Light On - 255"
+	echo "7) Light On - 128"
+	echo "8) Light On - 96"
+	echo "9) Light On - 64"
 	echo "q) Exit"
 	echo " "
 
 
 }
-
-#TTYUSB="/dev/ttyUSB1"
 #configure serial port, assuming /dev/ttyUSB1
-stty -F /dev/ttyUSB1 19200 cs8 -cstopb -parenb crtscts
+stty -F ${TTYUSB} 19200 cs8 -cstopb -parenb crtscts
 #stty -F /dev/ttyUSB1 19200 cs8 -cstopb -parenb
 #stty -F /dev/ttyUSB1 crtscts
 
-minicom -D /dev/ttyUSB1 -b 19200 -C /dev/null -o <<EOF
-!exit
-EOF
+#screen /dev/ttyUSB1 19200
+#socat -d -d /dev/ttyUSB1,raw,echo=0,b19200,cs8,crtscts -
+#minicom -D ${TTYUSB} -b 19200 -C /dev/null -o <<EOF
+#!exit
+#EOF
 
 #run menu loop until exit
 while true; do
@@ -43,16 +59,31 @@ while true; do
 
 	case $choice in
 		1)
-			echo "1001" > /dev/ttyUSB1
+			echo "1001" > ${TTYUSB}
 			;;
 		2)
-			echo "1000" > /dev/ttyUSB1
+			echo "1000" > ${TTYUSB}
 			;;
 		3)
-			echo "9999" > /dev/ttyUSB1 && echo "1001" > /dev/ttyUSB1 
+			echo "9999" > ${TTYUSB} && echo "1001" > ${TTYUSB} 
 			;;
 		4)
-			echo "1000" > /dev/ttyUSB1 && echo "255" > /dev/ttyUSB1
+			echo "1000" > ${TTYUSB} && echo "255" > ${TTYUSB}
+			;;
+		5)
+			echo "9999" > ${TTYUSB}
+			;;
+		6)
+			echo "255" > ${TTYUSB}
+			;;
+		7)
+			echo "128" > ${TTYUSB}
+			;;
+		8)
+			echo "96" > ${TTYUSB}
+			;;
+		9)
+			echo "64" > ${TTYUSB}
 			;;
 		q)
 			pkill minicom
